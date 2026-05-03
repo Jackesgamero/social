93b66b14ee51fc998c339aa3be048cef8fa8f817
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2026-05-04 00:17:06 +0200

    Authorization

--- internal/store/users.go
+++ internal/store/users.go
@@ -27,6 +27,8 @@ type User struct {
 	Password  password `json:"-"`
 	CreatedAt string   `json:"created_at"`
 	IsActive  bool     `json:"is_active"`
+	RoleID    int64    `json:"role_id"`
+	Role      Role     `json:"role"`
 }
 
 type password struct {
@@ -52,9 +54,10 @@ func (p *password) Compare(plainText string) error {
 
 func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error) {
 	query := `
-	  SELECT id, username, email, password, created_at
+	  SELECT users.id, username, email, password, created_at, roles.*
 	  FROM users
-	  WHERE id = $1 AND is_active = true
+	  JOIN roles ON (users.role_id = roles.id)
+	  WHERE users.id = $1 AND is_active = true
 	`
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
@@ -71,6 +74,10 @@ func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error) {
 		&user.Email,
 		&user.Password.hash,
 		&user.CreatedAt,
+		&user.Role.ID,
+		&user.Role.Name,
+		&user.Role.Level,
+		&user.Role.Description,
 	)
 	if err != nil {
 		switch err {
@@ -120,12 +127,18 @@ func (s *UserStore) GetByEmail(ctx context.Context, email string) (*User, error)
 
 func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 	query := `
-	  INSERT INTO users (username, password, email) VALUES($1, $2, $3) RETURNING id,
-	  created_at 
+	  INSERT INTO users (username, password, email, role_id) VALUES
+	  ($1, $2, $3, (SELECT id FROM roles WHERE name = $4)) 
+	  RETURNING id, created_at 
 	`
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
+
+	role := user.Role.Name
+	if role == "" {
+		role = "user"
+	}
 
 	err := tx.QueryRowContext(
 		ctx,
@@ -133,6 +146,7 @@ func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 		user.Username,
 		user.Password.hash,
 		user.Email,
+		role,
 	).Scan(
 		&user.ID,
 		&user.CreatedAt,
